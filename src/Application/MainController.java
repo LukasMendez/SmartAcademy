58b95f8e1ad0bc0562c31c79e69d6d82b665d65c
LukasMendez <43059840+LukasMendez@users.noreply.github.com>
2019-05-22 10:33:39 +0200

    - Testing merge

--- src/Application/MainController.java
+++ src/Application/MainController.java
@@ -2,11 +2,33 @@ package Application;
 
 import Persistance.DB;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableView;
+
 public class MainController {
 
     private DB db = DB.getInstance();
 
-    public void start(){
+    @FXML
+    private TableView educationMatrixTableView;
+
+    public void initialize() {
+
+
+        for (int i = 0; i < 10; i++) {
+
+
+
+
+        }
+
+    }
+
+
+
+
+    public void start() {
         db.connect();
         //db.test();
     }
