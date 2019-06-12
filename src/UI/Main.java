ff4a9acb310a88d04845cb29b1f9dd554c7d6eb0
LukasMendez <43059840+LukasMendez@users.noreply.github.com>
2019-06-12 10:45:52 +0200

    - Folders

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -1,18 +1,11 @@
 package UI;
 
-import com.sun.javafx.scene.control.skin.DatePickerSkin;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.DatePicker;
 import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import Application.MainController;
-
-import java.time.LocalDate;
 
 
 public class Main extends Application {
@@ -20,7 +13,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXML/MainWindow.fxml"));
         primaryStage.setTitle("SmartAcademy Manager");
         primaryStage.getIcons().add(new Image("UI/Images/iconPen.png"));
         primaryStage.setScene(new Scene(root, 1000, 800));
